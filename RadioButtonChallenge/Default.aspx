536c01757ee90ba062424d3f7e46fd681ff025c3
JoshBatchelor777 <joshbatchelor777@gmail.com>
2017-09-13 18:41:50 -0700

    First commit

---
+++ RadioButtonChallenge/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RadioButtonChallenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: larger;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h2>Your note taking preferences</h2>
+        <br />
+        <br />
+        <asp:RadioButton ID="pencilRB" runat="server" CssClass="auto-style1" GroupName="utencils" Text="Pencil" />
+        <br class="auto-style1" />
+        <asp:RadioButton ID="penRB" runat="server" CssClass="auto-style1" GroupName="utencils" Text="Pen" />
+    
+    </div>
+        <asp:RadioButton ID="phoneRB" runat="server" CssClass="auto-style1" GroupName="utencils" Text="Phone" />
+        <br class="auto-style1" />
+        <asp:RadioButton ID="tabletRB" runat="server" CssClass="auto-style1" GroupName="utencils" Text="Tablet" />
+        <br />
+        <br />
+        <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="OK" />
+        <br />
+        <br />
+        <asp:Image ID="resultImage" runat="server" />
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ RadioButtonChallenge/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Media;
+
+namespace RadioButtonChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+
+            /*
+            if (pencilRB.Checked)
+            {
+                resultLabel.Text = "You chose a Pencil";
+            }
+            else if (penRB.Checked)
+            {
+                resultLabel.Text = "You chose a Pen";
+            }
+            else if (phoneRB.Checked)
+            {
+                resultLabel.Text = "You chose a Phone";
+            }
+            else if (tabletRB.Checked)
+            {
+                //resultImage = tablet.png;
+                resultLabel.Text = "You chose a Tablet";
+            }
+            else
+            {
+                resultLabel.Text = "Please make a selection.";
+            }
+            */
+
+
+
+            if (pencilRB.Checked)
+                resultImage.ImageUrl = "~/pencil.png";
+            else if (penRB.Checked)
+                resultImage.ImageUrl = "~/pen.png";
+            else if (phoneRB.Checked)
+                resultImage.ImageUrl = "~/phone.png";
+            else if (tabletRB.Checked)
+                resultImage.ImageUrl = "~/tablet.png";
+            else
+                resultLabel.Text = "Please make a selection";
+        }
+    }
+}
